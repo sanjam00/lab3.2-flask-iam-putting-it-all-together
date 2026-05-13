2fe94bb439b78cee8b9e1751e7f5a49f627b5a0f
Sanaya James <sanaya.j.james@gmail.com>
2026-05-13 14:09:20 -0700

    implemented user and recipes classes, and userschema and recipeschema

--- server/models.py
+++ server/models.py
@@ -7,15 +7,59 @@ from config import db, bcrypt
 class User(db.Model):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', back_populates = 'user')
+
+    @hybrid_property
+    def password_hash(self):
+        return AttributeError('Password hashes may not be viewed.')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.ecode('utf-8')
+        )
+
+    def __repr__(self):
+        return f'<Username {self.username}. ID: {self.id}>'
 
 class Recipe(db.Model):
     __tablename__ = 'recipes'
+    __table_args__ = (
+        db.CheckConstraint('length(instructions) >= 50')
+    )
     
-    pass
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    user = db.relationship('User', back_populates = 'recipes')
 
 class UserSchema(Schema):
-    pass
+    id = fields.Int()
+    username = fields.String()
+    image_url = fields.String()
+    bio = fields.String()
+
+    recipes = fields.List(fields.Nested(lambda: RecipeSchema(exclude=('user',))))
 
 class RecipeSchema(Schema):
-    pass
+    id = fields.Int()
+    title = fields.String()
+    instructions = fields.String()
+    minutes_to_completed = fields.Int()
+
+    user = fields.Nested(UserSchema(exclude=('recipes',)))
